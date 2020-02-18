1f9b4e046a576b3ab70c5b1966a145d96b649d52
gsergey99 <gsergey99>
2020-02-18 01:30:43 +0100

    Terminar metodo copia de PB. Observar flags de open().

--- Práctica 1/PB.c
+++ Práctica 1/PB.c
@@ -8,6 +8,12 @@
 #include <errno.h>
 #include <string.h>
 #include <ctype.h>
+#include <fcntl.h>
+
+
+
+
+void copy_file(char *source, char *destiny);
 
 
 int main(int argc, char *argv[]){
@@ -26,33 +32,58 @@ if (file_name ==NULL){
 }
 
 while(fscanf(file_name,"%s",buffer)!=EOF){
-    if(count%3==0){
-        *DNI = buffer;
-    }else if (count%==0){
-        model = buffer;
-    }else{
-        count=3;
-        switch (model)
+    
+        if(strcmp(buffer,"A")==0){
+            copy_file("MODELOSEXAMEN/MODELOA.pdf","HOLA/MODELOA.pdf");
+        }else if (strcmp(buffer,"B")==0)
         {
-        case 'A':
-            
-            break;
-        
-        case 'B':
-            break;
-        
-        case 'C':
+            printf("Soy B");
 
+            //copy_file("/MODELOSEXAMEN/MODELOB.pdf","HOLA");
+
+        }else if (strcmp(buffer,"C")==0){    
+                
+            printf("Soy C");
+
+            //copy_file("/MODELOSEXAMEN/MODELOC.pdf","HOLA")
+   
         }
-
-    }
-    
-    
-    printf("%s\n", buffer);
-    count++;
+            printf("%s\n", buffer);
 }
 fclose(file_name);
-printf("Creación de directorios finalizada.\n");
 return EXIT_SUCCESS;
 
+
 }
+
+void copy_file(char *src, char *dest){
+    int src_fd, dst_fd, n, err;
+    unsigned char buffer[4096];
+    char * src_path, dst_path;
+
+    src_fd = open(src, O_RDONLY);
+    dst_fd = open(dest, O_CREAT | O_WRONLY);
+
+    while (1) {
+        err = read(src_fd, buffer, 4096);
+        if (err == -1) {
+            printf("Error reading file.\n");
+            exit(1);
+        }
+        n = err;
+
+        if (n == 0) break;
+
+        err = write(dst_fd, buffer, n);
+        if (err == -1) {
+            printf("Error writing to file.\n");
+            exit(1);
+        }
+    }
+
+    close(src_fd);
+    close(dst_fd);
+}
+
+
+
