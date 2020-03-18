43a1449d8c366a2841b41aa9be5bfe61a4f50e1c
gsergey99 <gsergey99>
2020-03-18 12:47:58 +0100

    Funcion encontrar palabras realizada. Continuar con funcion de leer en lineas diferentes

--- practica2/src/main.cpp
+++ practica2/src/main.cpp
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include "LineaResultado.h"
 #include <string.h>
+#include <regex>
 
 using namespace std;
 
 int contar_filas(std::string fichero);
 void encontrar_palabra (std::string fichero, std::string palabra);
-
-
+void leer_line();
+    
+    
 int main(int argc, char *argv[])
 {
     
@@ -22,7 +24,7 @@ int main(int argc, char *argv[])
     std::string palabra = argv[2];
     int num_hilos = atoi(argv[3]);
     int num_filas = contar_filas(fichero);
-    encontrar_palabra ("test.txt", "fidelidad.");
+    encontrar_palabra ("test.txt", "fidelidad");
 
     
     
@@ -56,9 +58,41 @@ int contar_filas(std::string fichero){
 void encontrar_palabra (std::string fichero, std::string palabra){
     
     ifstream src_fichero;
+    std::string expr_regular = palabra+ "[.,:?!]";
+    std::regex palabra_comp(expr_regular, std::regex_constants::ECMAScript | std::regex_constants::icase);
+    std::string linea;
     std::string caracter;
+    std::string palabra_anterior;
+    std::string palabra_posterior;
 
     src_fichero.open(fichero); //src/test.txt
+    if (src_fichero.fail()) {
+        cerr << "Error al abrir el fichero de entrada" << endl;
+        src_fichero.close();
+        exit(EXIT_FAILURE);
+    }
+    if(src_fichero.is_open()){
+        while(getline(src_fichero,linea)){
+            if(std::regex_search(linea,palabra_comp)){
+                caracter = std::regex_replace(linea,palabra_comp,"\e[3m$&\e[O");
+                cout << "Palabra "<< caracter << endl;
+
+            }
+        }
+    }
+    
+    src_fichero.close();
+    
+}
+    
+void leer_linea(){
+    /*ifstream src_fichero;
+    char buffer[4096];
+    std::string palabra_anterior;
+    std::string palabra_posterior;
+    int linea =0;
+
+    src_fichero.open("test.txt"); //src/test.txt
     if (src_fichero.fail()) {
         cerr << "Error opeing a file" << endl;
         src_fichero.close();
@@ -66,12 +100,11 @@ void encontrar_palabra (std::string fichero, std::string palabra){
     }
     
     while (!src_fichero.eof()) {
-    src_fichero >> caracter;
-    if(caracter.compare(palabra)==0){
+        src_fichero.seekg(buffer);
+        src_fichero >> caracter;
         cout << caracter << endl;
-    }
+    
+    
   }
-    src_fichero.close();
-    
+    src_fichero.close();*/
 }
-    
